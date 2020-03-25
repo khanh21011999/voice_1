9b26ffbf6e0bcdbe435f77a940355840115ac3e2
quockhanh21299 <37074707+quockhanh21299@users.noreply.github.com>
2020-03-25 13:50:39 +0700

    update

--- testPy.py
+++ testPy.py
@@ -4,44 +4,44 @@ from pydub.silence import detect_nonsilent
 import speech_recognition as sr
 
 r = sr.Recognizer()
-r.pause_threshold = 3
+r.pause_threshold = 5
 '''
 with sr.Microphone() as source:
     try:
-      print("taliking after 2 second")
+      print("Maximum wait is 2 second")
       audio=r.listen(source,timeout=2)
-      print(r.recognize_google(audio,language='vi-VN'))
     except sr.WaitTimeoutError:
         print("waitError")
 with open("test1.wav","wb") as f:
     f.write(audio.get_wav_data())
 '''
-sound = AudioSegment.from_file('untitled.wav',format="wav")
+sound = AudioSegment.from_file('thegioi.wav',format="wav")
 chucks = split_on_silence(sound,
                           min_silence_len=1000,
-                          silence_thresh=-54,
-                          keep_silence=0,
+                          silence_thresh=-50,
+                          keep_silence=1000,
                           seek_step=1)
-print(sound.dBFS)
-print(detect_nonsilent(sound, 1000, -54, 1))
+##print(sound.dBFS)
+##print(detect_nonsilent(sound, 1000, -55, 1))
+with open('text_document.txt','r',encoding='utf-8') as read_file:
+    file=read_file.read()
+    print(file)
+with open('thegioi.txt','a',encoding='utf-8') as file:
+    file.write('https://vnexpress.net/the-gioi/tiem-kich-italy-duoi-theo-may-bay-duc-vi-su-co-mat-lien-lac-3907903.html'+'\n')
 
-
-
-'''
 for i, chuck in enumerate(chucks):
-    out_file = "D:\python_project\XLTN\project_1\\thoi_su\chuck{0}.wav".format(i)
+    out_file = "D:\python_project\XLTN\project_1_XLTN\\thegioi{0}.wav".format(i)
     chuck.export(out_file, bitrate='192k', format="wav")
-    filename = 'chuck' + str(i) + '.wav'
+    filename = 'thegioi'+ str(i) + '.wav'
     file=filename
     with sr.AudioFile(file) as source:
         audio=r.listen(source)
     try:
         rec=r.recognize_google(audio,language='vi-VN')
-        with open('thoisu.txt','a',encoding='utf=8') as the_file:
+        with open('thegioi.txt','a',encoding='utf=8') as the_file:
 
             the_file.write(filename+'\n'+rec+'\n')
     except sr.UnknownValueError:
         print("Could not understand what you said")
     except sr.RequestError as e:
         print("Could not request")
-'''
