58d992ffc0cbfcc505351fcfe7e56e6d21231ccd
quockhanh21299 <37074707+quockhanh21299@users.noreply.github.com>
2020-03-27 02:40:14 +0700

    update
    add more detail

--- testPy.py
+++ testPy.py
@@ -1,6 +1,7 @@
+# ---------lê quốc khánh mssv:17021272 27/03/2020-----------
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-#from pydub.silence import detect_nonsilent
+# from pydub.silence import detect_nonsilent
 import speech_recognition as sr
 import soundfile as sf
 import sounddevice as sd
@@ -8,31 +9,36 @@ import sys
 import queue
 from sounddevice import default
 import numpy
+
 assert numpy
 ##record audio tham khảo tại https://github.com/spatialaudio/python-sounddevice/blob/master/examples/rec_unlimited.py
 q = queue.Queue()
+# văn bản cần đọc
 with open('text_document.txt', 'r', encoding='utf-8') as read_file:
     file = read_file.read()
     print(file)
-filename = 'thogioi6.wav'
+################
+filename = 'thogioi6.wav'  # filename là tên file và không được trùng
 samplerate = 44100
+
+
 def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
     if status:
         print(status, file=sys.stderr)
-    q.put(indata.copy())
+    q.put(indata.copy())  ##tạo ra vùng trống để có thể chứa được dữ liệu audio
 
 
 try:
     with sf.SoundFile(filename, mode='x', samplerate=samplerate, channels=1) as soundFlie:
         with sd.InputStream(samplerate=samplerate, device=default.device, channels=1, callback=callback):
-            print('>>>>>>press Ctrl+C to stop<<<<<<<')
+            print('>>>>>>press Ctrl+C to stop<<<<<<<')  # keyboardInterupt mặc định là Ctrl+C
             while True:
-                soundFlie.write(q.get())
+                soundFlie.write(q.get())  ##lấy data từ hàng đợi
 except KeyboardInterrupt:
-    print('\n Recorded '+repr(filename))
-r = sr.Recognizer()
+    print('\n Recorded ' + repr(filename))
+
 '''
+
 r.pause_threshold = 5 ## thời gian ngưng nhận âm tối đa để dừng
 with sr.Microphone() as source:
     try:
@@ -49,11 +55,14 @@ chucks = split_on_silence(sound,
                           silence_thresh=-50,  ##ngưỡng im lặng được xem là im lặng (đơn vị dBFS)
                           keep_silence=1000,  ##khoang im lặng sau khi tách câu phát âm
                           seek_step=1)
-print(sound.dBFS)
+##print(sound.dBFS)
 ##print(detect_nonsilent(sound, 1000, -55, 1))
 
 with open('thegioi.txt', 'a', encoding='utf-8') as file:
-    file.write('https://vnexpress.net/the-gioi/tiem-kich-italy-duoi-theo-may-bay-duc-vi-su-co-mat-lien-lac-3907903.html' + '\n')
+    file.write(
+        'https://vnexpress.net/the-gioi/tiem-kich-italy-duoi-theo-may-bay-duc-vi-su-co-mat-lien-lac-3907903.html' + '\n')
+###hàm tách câu phát âm dựa trên  khoảng im lặng
+r = sr.Recognizer()
 for i, chuck in enumerate(chucks):
     out_file = "D:\python_project\XLTN\project_1_XLTN\\thegioi{0}.wav".format(i)
     chuck.export(out_file, bitrate='192k', format="wav")
@@ -64,9 +73,9 @@ for i, chuck in enumerate(chucks):
     try:
         rec = r.recognize_google(audio, language='vi-VN')
         with open('thegioi.txt', 'a', encoding='utf=8') as the_file:
-
+            ## ghi text vào nhớ speech_to_text của google
             the_file.write(filename + '\n' + rec + '\n')
     except sr.UnknownValueError:
-        print("Could not understand what you said")
+        print("Could not understand what you said") #lỗi không nhận dạng được
     except sr.RequestError as e:
-        print("Could not request")
+        print("Could not request") #không nối được máy chủ
